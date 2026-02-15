16b1a9c9671045ccd98c1118d73dfd1d8eaf6f43
brunaoarrudao-hue <brunaoarrudao@gmail.com>
2026-02-15 06:13:34 -0300

    Update Lemon.lua

--- Lemon.lua
+++ Lemon.lua
@@ -193,8 +193,7 @@ Tab1:CreateToggle({
    Name = "Matar NPCs Próximos",
    CurrentValue = false,
    Callback = function(v) _G.AutoFarm = v end,
-})
-FarmTab:CreateToggle({
+Tab1:CreateToggle({
    Name = "Auto Chest (Farm de Dinheiro)",
    CurrentValue = false,
    Callback = function(v) 
